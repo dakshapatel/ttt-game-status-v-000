e7ed24c118935ff7aa7f13bef542f495bf6528ce
Daksha Patel <dakshapatel100@gmail.com>
2018-04-27 21:20:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -64,7 +64,7 @@ def draw?(board)
 end
 
 def over?(board)
-if won?(board) == true || draw?(board) == true || full?(board) == true
+if won?(board) == true || draw?(board) == true || full?(board) == true || won?(board) !=nil
   return true
 end
 end

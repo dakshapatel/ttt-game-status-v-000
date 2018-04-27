f242795926c64036c2d6c9736e0429d3c2784fb7
Daksha Patel <dakshapatel100@gmail.com>
2018-04-27 19:07:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,6 +17,6 @@ WIN_COMBINATIONS = [
 
 def won?(board)
    WIN_COMBINATIONS.find do |win_comb|
-     board[win_comb[0]] == board[win_comb[1]] == board[win_comb[2]]
+     board[win_comb[0]] == board[win_comb[1]] && board[win_comb[2]]
   end
 end

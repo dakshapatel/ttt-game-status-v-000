8198e7b91282d9cdeef4284fb5fd74d875628594
Daksha Patel <dakshapatel100@gmail.com>
2018-04-27 18:44:13 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,3 +9,13 @@ def won(board)
   if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
   "X won in the top row"
 end
+  middle_row_win = [3,4,5]
+  if board[middle_row_win[3]] == "X" && board[middle_row_win[4]] == "X" && board[middle_row_win[5]] == "X"
+
+  bottom_row_win = [6,7,8]
+  if board[bottome_row_win[0]] == "X" && board[bottom_row_win[1]] == "X" && board[bottom_row_win[2]] == "X"
+
+
+
+
+end

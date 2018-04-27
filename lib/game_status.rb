a746cbc39e0bd20280ba77fa00e836b33492615c
Daksha Patel <dakshapatel100@gmail.com>
2018-04-27 18:48:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,19 +4,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-def won(board)
-  top_row_win = [0,1,2]
-
-  if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
-  "X won in the top row"
-
-  middle_row_win = [3,4,5]
-  if board[middle_row_win[3]] == "X" && board[middle_row_win[4]] == "X" && board[middle_row_win[5]] == "X"
-
-  bottom_row_win = [6,7,8]
-  if board[bottome_row_win[6]] == "X" && board[bottom_row_win[7]] == "X" && board[bottom_row_win[8]] == "X"
-
-WIN_COMBINATIONS = [
+def WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
   [6,7,8],
@@ -27,7 +15,3 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
   end
-  end
-  end
-
-end
